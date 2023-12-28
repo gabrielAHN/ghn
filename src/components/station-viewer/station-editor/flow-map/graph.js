c152de4f57f98b8a4a7abffb0ce77996d030af44
gh15hidalgo <gabrielhn@hey.com>
2023-12-28 08:36:36 -0500

    fix

---
+++ src/components/station-viewer/station-editor/flow-map/graph.js
@@ -0,0 +1,49 @@
+import ELK from 'elkjs/lib/elk.bundled.js';
+// import { initialNodes, initialEdges } from "./initialData";
+
+const elk = new ELK();
+
+
+const elkLayout = (flowdata) => {
+
+    const graph = {
+        id: "root",
+        layoutOptions: {
+            'considerModelOrder.strategy': 'PREFER_EGDES',
+            "elk.algorithm": "layered",
+            "elk.direction": "LEFT",
+            'elk.alignment': 'LEFT',
+            'elk.edgeRouting': 'POLYLINE',
+            'elk.layered.nodePlacement.strategy': 'BRANDES_KOEPF',
+            // "elk.alg.mrtree.options.OrderWeighting": "CONSTRAINT",
+            // "elk.order.layoutHierarchy": 'true',
+            'elk.partitioning.activate': 'true',
+            'elk.layered.mergeEdges': 'true',
+            'elk.layered.layering.strategy': 'BF_MODEL_ORDER',
+            // 'elk.spacing.nodeNode': '1',
+            'elk.layered.spacing.edgeNodeBetweenLayers': '100',
+            'elk.layered.spacing.nodeNodeBetweenLayers': '100',
+            
+            // intCoordinates: true,
+            // direction: "DOWN",
+            // edgeRouting: "ORTHOGONAL",
+            // cycleBreaking: "INTERACTIVE",
+            // nodeLayering: "INTERACTIVE",
+            // crossMin: "INTERACTIVE",
+            
+            // "elk.hierarchyHandling": "SEPARATE_CHILDREN",
+            // "elk.hierarchyHandling": "INHERIT",
+            // 'elk.layered.spacing.nodeNodeBetweenLayers': '1',
+            // 'elk.layered.unnecessaryBendpoints': 'true',
+            // 'elk.spacing.nodeNode': '1',
+            // "nodePlacement.strategy": "SIMPLE"
+        },
+
+        children: flowdata.nodes,
+        edges: flowdata.edges
+    };
+    // console.log(graph)
+    return elk.layout(graph);
+};
+
+export default elkLayout;
