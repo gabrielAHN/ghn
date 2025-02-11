adc6e2e52107a26d03fd43df1912400bcf2f3469
gh15hidalgo <gabrielhn@hey.com>
2025-02-11 21:19:18 +0800

    fix home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,25 @@
-'use client';
+"use client";
 
 import { useState, useEffect } from "react";
 
-import Me from '@/assets/Home/me.png';
-import Wulfz from '@/assets/Home/wulfz.gif';
+import Me from "@/assets/Home/me.png";
+import Wulfz from "@/assets/Home/wulfz.gif";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 
-
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
-import Work from "./component/WorkSection"
-import Bio from "./component/BioSection"
-
+import Work from "./component/WorkSection";
+import Bio from "./component/BioSection";
 
 export default function Home() {
   const [count, setCount] = useState(0);
   const [photo, setPhoto] = useState(Me);
 
   useEffect(() => {
-    if (count == 3) {
+    if (count === 3) {
       setPhoto(Wulfz);
-    }
-    else if (count > 3) {
+    } else if (count > 3) {
       setPhoto(Me);
       setCount(0);
     }
@@ -30,48 +27,44 @@ export default function Home() {
 
   const SectionData = [
     {
-      "id": "work",
-      "label": "Work",
-      "component": (<Work />)
-
+      id: "work",
+      label: "Work",
+      component: <Work />,
     },
     {
-      "id": "bio",
-      "label": "Bio",
-      "component": (<Bio />)
-
+      id: "bio",
+      label: "Bio",
+      component: <Bio />,
     },
     {
-      "id": "contact",
-      "label": "Contact",
-      "component":
-        (
-          <div>
+      id: "contact",
+      label: "Contact",
+      component: (
+        <div>
           <Link
-          href={'https://github.com/gabrielAHN'}
-          rel="noopener noreferrer"
-          target="_blank"
-          className="h-[20vh] w-20 mb-[10vh]"
-          >
-        <Button variant={"icon"} className="h-10">
-          <FaGithub className="h-[20vh] w-[20vh]"/>
-        </Button>
-          </Link>
-            <Link
-            href={'https://linkedin.com/in/gabriel-hidalgo-41742867'}
+            href={"https://github.com/gabrielAHN"}
             rel="noopener noreferrer"
             target="_blank"
             className="h-[20vh] w-20 mb-[10vh]"
-            >
-          <Button variant={"icon"} className="h-10">
-            <FaLinkedin className="h-[20vh] w-[20vh]"/>
-          </Button>
+          >
+            <Button variant="icon" className="h-10">
+              <FaGithub className="h-[20vh] w-[20vh]" />
+            </Button>
+          </Link>
+          <Link
+            href={"https://linkedin.com/in/gabriel-hidalgo-41742867"}
+            rel="noopener noreferrer"
+            target="_blank"
+            className="h-[20vh] w-20 mb-[10vh]"
+          >
+            <Button variant="icon" className="h-10">
+              <FaLinkedin className="h-[20vh] w-[20vh]" />
+            </Button>
           </Link>
         </div>
-        )
-    }
-  ]
-
+      ),
+    },
+  ];
 
   return (
     <div className="w-full h-full">
@@ -81,32 +74,17 @@ export default function Home() {
           src={photo}
           alt="Cities Lover icon"
           onClick={() => setCount(count + 1)}
-          className="
-            h-[42vh]
-            w-[42vh]
-            object-cover
-            rounded-full
-          "
+          className="h-[42vh] w-[42vh] object-cover rounded-full"
         />
       </div>
-      <div
-        className="
-          items-center justify-center
-          w-full h-full
-          text-center
-        "
-      >
-        <h1 className="font-extrabold text-4xl">
-          Gabriel Hidalgo ~ Naranjo
-        </h1>
-        <h4 className="text-xl">
-          Art 🧑‍🎨, Cities 🌇, and Tech 🦾
-        </h4>
+
+      <div className="items-center justify-center w-full h-full text-center">
+        <h1 className="font-extrabold text-4xl">Gabriel Hidalgo ~ Naranjo</h1>
+        <h4 className="text-xl">Art 🧑‍🎨, Cities 🌇, and Tech 🦾</h4>
         <div className="mt-5 mb-5">
-          {SectionData.map(section => (
+          {SectionData.map((section) => (
             <a
               key={section.id}
-              id={section.id}
               className="mx-2 text-3xl font-semibold hover:[text-shadow:_0.5vh_0.4vh_0_rgb(255_220_44_/_100%)]"
               href={`#${section.id}`}
             >
@@ -114,8 +92,8 @@ export default function Home() {
             </a>
           ))}
         </div>
-        <div >
-          {SectionData.map(section => (
+        <div>
+          {SectionData.map((section) => (
             <div
               key={section.id}
               id={section.id}
